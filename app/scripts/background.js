6f4a1e56430714a456f2b529110ef707eb292a4b
lubosch <lubomir.vnenk@zoho.com>
2015-08-28 00:59:09 +0200

    extension logger advanced

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,36 +1,81 @@
+var current_id = -1;
+var current_urls = {};
+var current_url = '';
+var chrome_active = false;
+
 //New tab and changes within tab url
-chrome.tabs.onUpdated.addListener(function() {
+chrome.tabs.onUpdated.addListener(function () {
     notifyTabChanged()
 });
 
 // Switching tabs
-chrome.tabs.onActivated.addListener(function() {
+chrome.tabs.onActivated.addListener(function () {
     // Send a message to the active tab
     notifyTabChanged()
 });
 
 // Listener for messages
 chrome.runtime.onMessage.addListener(
-    function(request) {
-        if( request.message === "tab_changed_url" ) {
-            console.log(request.url);
-            sendToServer({"new_url": request.url}, 'tab_changed')
+    function (request) {
+        if (request.message === "tab_changed_url") {
+            if (current_url != request.url) {
+                current_url = request.url;
+                sendToServer(request, 'active_page/new_page')
+            }
+        } else if (request.message === 'active_status') {
+            if (request.active && !chrome_active) {
+                sendToServer({}, 'chrome_activated');
+                chrome_active = true;
+            }else if(!request.active && chrome_active){
+                sendToServer({}, 'chrome_closed');
+                chrome_active = false;
+            }
         }
     }
 );
 
+
 // notify content script url has changed
 function notifyTabChanged() {
 //     Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed"});
+        if (current_url != activeTab.url) {
+            if (current_id != activeTab.id && current_urls[activeTab.id] == activeTab.url) {
+                sendToServer({tab_id: activeTab.id}, 'tab_change');
+                current_url = activeTab.url
+            } else {
+                chrome.tabs.sendMessage(activeTab.id, {
+                    "message": "tab_changed",
+                    "tab_id": activeTab.id,
+                    "previous_tab_id": current_id,
+                    "previous_url": current_url
+                });
+            }
+            console.log(activeTab.id)
+            current_id = activeTab.id;
+            current_urls[activeTab.id] = activeTab.url;
+        }
     });
 }
 
 // send data to server action
-function sendToServer(send_data, action){
-    $.post('http://localhost:3000/extension_api/' + action, send_data, function(data){
+function sendToServer(send_data, action) {
+    $.post('http://localhost:3000/extension_api/' + action, send_data, function (data) {
 
     });
 }
+
+chrome.alarms.create('active-window', {'periodInMinutes': 0.1});
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        if (!tabs[0] && chrome_active == true) {
+            sendToServer({}, 'chrome_closed');
+            chrome_active = false;
+        } else if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                "message": "active_status"
+            });
+        }
+    });
+});
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,8 +1,44 @@
 chrome.runtime.onMessage.addListener(
     function (request) {
-        if( request.message === "tab_changed" ) {
-            var currentUrl = $(location).attr('href');
-            chrome.runtime.sendMessage({"message": "tab_changed_url", "url": currentUrl});
+        var currentUrl = $(location).attr('href');
+        if (request.message === "tab_changed" && currentUrl != request.previous_url) {
+            var title = $(document).find("title").text();
+            var headers = joinDivsText($('h1'));
+            var meta_description = $("meta[property='og:description']").attr("content") || $("meta[name='description']").attr("content")
+
+            var corpus = joinDivsText(getTextNodesIn('div'));
+            var tfidf = analyze_web_text(corpus);
+            var referrer =  document.referrer;
+
+            console.log('change 3 ' + currentUrl);
+            chrome.runtime.sendMessage({
+                "message": "tab_changed_url",
+                "url": currentUrl,
+                "tfidf": tfidf,
+                "title": title,
+                "headers": headers,
+                "tab_id": request.tab_id,
+                "previous_tab_id": request.previous_tab_id,
+                "previous_url": request.previous_url,
+                "referrer ": referrer ,
+                "meta_description": meta_description
+            });
+            console.log('change 2 ' + currentUrl);
+
+        } else if (request.message === 'active_status') {
+            chrome.runtime.sendMessage({
+                message: 'active_status',
+                active: document.hasFocus()
+            });
         }
     }
 );
+
+function joinDivsText(divs) {
+    return $.map(
+        divs,
+        function (element) {
+            return $(element).text()
+        }
+    ).join(" ");
+}
